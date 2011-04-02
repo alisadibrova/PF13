2f8ae1faed61ec23e84ce6240ea4f6bf9a896035
Alisa <Alisa@Alisa-PC>
2011-04-02 15:47:18 +0200

    Signed-off-by: Alisa <Alisa@Alisa-PC>

---
+++ pfi3_assignment_1/pfi3_assignment_2/src/is/mah/k3/dibrova/alisa/pfi3_assignment_2/Pfi3_assignment_2.java
@@ -0,0 +1,13 @@
+package is.mah.k3.dibrova.alisa.pfi3_assignment_2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Pfi3_assignment_2 extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
